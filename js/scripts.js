d20f5508d30c5631c2a650d010a98f114e7dccd4
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-08-01 18:56:13 -0400

    CSS refactor of CTA and sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 const navBar = document.querySelector('.navigation');
-const navLogoMic = document.querySelector('.logo-mic')
 console.log(navBar);
 
 
@@ -7,7 +6,6 @@ console.log(navBar);
 window.onscroll = () => {
     if(window.pageYOffset > 10){
         navBar.classList.add('solid-bg');
-        navLogoMic.classList.add('logo-mic-min');
     }else{
         navBar.classList.remove('solid-bg')
     }
